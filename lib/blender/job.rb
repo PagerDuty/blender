b87f6a0f6f5e60cccd900ac65dcba6ade4835892
Ranjib Dey <ranjib@pagerduty.com>
2014-07-05 19:51:08 +0000

    compute job names automatically

--- lib/blender/job.rb
+++ lib/blender/job.rb
@@ -1,19 +1,41 @@
-
-
 module Blender
   class Job
 
-    attr_reader :tasks, :hosts
+    attr_reader :tasks, :hosts, :driver
 
-    def initialize(id, name, hosts, tasks)
+    def initialize(id, default_driver, hosts, tasks)
       @id = id
-      @name = name
+      @name = compute_name(Array(hosts), Array(tasks))
+      @default_driver = default_driver
       @hosts = Array(hosts)
       @tasks = Array(tasks)
+      task_drivers =  Array(tasks).collect(&:driver).compact.uniq
+      if task_drivers.size == 1
+        @driver = task_drivers.first
+      elsif task_drivers.empty?
+        @driver = default_driver
+      else
+        raise 'Job container tasks with heretogenous drivers'
+      end
     end
 
     def to_s
       "Job[#{@name}]"
+    end
+
+    private
+    def compute_name(hosts, tasks)
+      if tasks.size == 1
+        t_part = tasks.first.name
+      else
+        t_part = "#{tasks.size} tasks"
+      end
+      if hosts.size == 1
+        h_part = hosts.first
+      else
+        h_part = "#{hosts.size} members"
+      end
+      "#{t_part} on #{h_part}"
     end
   end
 end
