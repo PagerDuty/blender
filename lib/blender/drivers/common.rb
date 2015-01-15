42abddcb6c80dbb6a5ba9ee894daba64cf6fccce
Smit Shah <who828@gmail.com>
2015-01-15 12:42:00 +0530

    Pulled out duplicate code to a common module

---
+++ lib/blender/drivers/common.rb
@@ -0,0 +1,26 @@
+module Common
+  def remote_exec(command, session)
+    session.open_channel do |ch|
+      ch.request_pty
+      ch.exec(command) do |ch, success|
+        unless success
+          Log.debug("Command not found:#{success.inspect}")
+          exit_status = -1
+        end
+        ch.on_data do |c, data|
+          stdout << data
+          c.send_data("#{password}\n") if data =~ /^blender sudo password: /
+        end
+        ch.on_extended_data do |c, type, data|
+          stderr << data
+        end
+        ch.on_request "exit-status" do |ichannel, data|
+          l = data.read_long
+          exit_status = [exit_status, l].max
+          Log.debug("exit_status:#{exit_status} , data:#{l}")
+        end
+      end
+      Log.debug("Exit(#{exit_status}) Command: '#{command}'")
+    end
+  end
+end
--- lib/blender/drivers/ssh.rb
+++ lib/blender/drivers/ssh.rb
@@ -18,11 +18,13 @@
 require 'net/ssh'
 require 'blender/exceptions'
 require 'blender/drivers/base'
+require 'blender/drivers/common'
 
 module Blender
   module Driver
     class Ssh < Base
       attr_reader :user
+      include Common
 
       def initialize(config = {})
         cfg = config.dup
@@ -49,28 +51,7 @@ module Blender
         password = config[:password]
         command = fixup_sudo(command)
         exit_status = 0
-        channel = session.open_channel do |ch|
-          ch.request_pty
-          ch.exec(command) do |ch, success|
-            unless success
-              Log.debug("Command not found:#{success.inspect}")
-              exit_status = -1
-            end
-            ch.on_data do |c, data|
-              stdout << data
-              c.send_data("#{password}\n") if data =~ /^blender sudo password: /
-            end
-            ch.on_extended_data do |c, type, data|
-              stderr << data
-            end
-            ch.on_request "exit-status" do |ichannel, data|
-              l = data.read_long
-              exit_status = [exit_status, l].max
-              Log.debug("exit_status:#{exit_status} , data:#{l}")
-            end
-          end
-          Log.debug("Exit(#{exit_status}) Command: '#{command}'")
-        end
+        channel = remote_exec(command, session)    
         channel.wait
         ExecOutput.new(exit_status, stdout, stderr)
       end
